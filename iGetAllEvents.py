3cb564927c90a21608e3912642bb7d9bfccafae2
Thanawat Bumpengpun <76035626+0736b@users.noreply.github.com>
2021-06-28 15:47:54 +0700

    Update iGetAllEvents.py

--- iGetAllEvents.py
+++ iGetAllEvents.py
@@ -17,26 +17,26 @@ async def on_ready():
 @client.event
 async def on_message(message):
     #Edit message at here
-    if '**FISH**' in message.content and message.author.id == here:
+    if 'here' in message.content and message.author.id == hereIsID:
         print('ได้รับข้อความ A MEGALODON HAS SPAWNED IN THE RIVER')
         time.sleep(3)
-        await message.channel.send('FISH')
+        await message.channel.send('FISH') #This is message that reply when get event
         print('ส่งข้อความ FISH สำเร็จ')
         answer = "fish"
         return answer
 
-    elif '**CATCH**' in message.content and message.author.id == here:
+    elif 'here' in message.content and message.author.id == hereIsID:
         print('ได้รับข้อความ IT''S RAINING COINS')
         time.sleep(3)
-        await message.channel.send('CATCH')
+        await message.channel.send('CATCH') #This is message that reply when get event
         print('ส่งข้อความ CATCH สำเร็จ')
         answer = "catch"
         return answer
 
-    elif '**CHOP**' in message.content and message.author.id == here:
+    elif 'here' in message.content and message.author.id == hereIsID:
         print('ได้รับข้อความ AN EPIC TREE HAS JUST GROWN')
         time.sleep(3)
-        await message.channel.send('CHOP')
+        await message.channel.send('CHOP') #This is message that reply when get event
         print('ส่งข้อความ CHOP สำเร็จ')
         answer = "chop"
         return answer
